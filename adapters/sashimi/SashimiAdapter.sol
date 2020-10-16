9019dddf93cee7999e102fe86b4b0a0fc1f9e8d2
Aaron Zinghini <aaron.zinghini@seeingmachines.com>
2020-10-16 13:50:41 -0700

    Minor warnings cleanup

--- adapters/sashimi/SashimiAdapter.sol
+++ adapters/sashimi/SashimiAdapter.sol
@@ -11,85 +11,82 @@ import "../../IDrainController.sol";
 import "./IMasterChef.sol";
 
 contract SashimiAdapter is IVampireAdapter {
-    IDrainController constant drainController = IDrainController(0x2e813f2e524dB699d279E631B0F2117856eb902C);
-    IMasterChef constant sashimiMasterChef = IMasterChef(0x1daed74ed1dd7c9dabbe51361ac90a69d851234d);
-    IERC20 constant sashimi = IERC20(0xc28e27870558cf22add83540d2126da2e4b464c2);
-    IERC20 constant weth = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    IUniswapV2Pair constant wethSashimiPair = IUniswapV2Pair(0x3fa4b0b3053413684d0b658689ede7907bb4d69d);
-    // token 0 - weth
-    // token 1 - sashimi
-
-    constructor() public {
-    }
+    IDrainController constant DRAIN_CONTROLLER = IDrainController(0x2e813f2e524dB699d279E631B0F2117856eb902C);
+    IMasterChef constant SASHIMI_MASTERCHEF = IMasterChef(0x1DaeD74ed1dD7C9Dabbe51361ac90A69d851234D);
+    IERC20 constant SASHIMI = IERC20(0xC28E27870558cF22ADD83540d2126da2e4b464c2);
+    IERC20 constant WETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IUniswapV2Pair constant WETH_SASHIMI_PAIR = IUniswapV2Pair(0x3fA4B0b3053413684d0B658689Ede7907bB4D69D);
+    // token 0 - WETH
+    // token 1 - SASHIMI
 
     // Victim info
     function rewardToken() external view override returns (IERC20) {
-        return sashimi;
+        return SASHIMI;
     }
 
     function poolCount() external view override returns (uint256) {
-        return sashimiMasterChef.poolLength();
+        return SASHIMI_MASTERCHEF.poolLength();
     }
 
     function sellableRewardAmount() external view override returns (uint256) {
         return uint256(-1);
     }
-    
+
     // Victim actions, requires impersonation via delegatecall
     function sellRewardForWeth(address, uint256 rewardAmount, address to) external override returns(uint256) {
-        require(drainController.priceIsUnderRejectionTreshold(), "Possible price manipulation, drain rejected");
-        sashimi.transfer(address(wethSashimiPair), rewardAmount);
-        (uint wethReserve, uint sashimiReserve,) = wethSashimiPair.getReserves();
+        require(DRAIN_CONTROLLER.priceIsUnderRejectionTreshold(), "Possible price manipulation, drain rejected");
+        SASHIMI.transfer(address(WETH_SASHIMI_PAIR), rewardAmount);
+        (uint wethReserve, uint sashimiReserve,) = WETH_SASHIMI_PAIR.getReserves();
         uint amountOutput = UniswapV2Library.getAmountOut(rewardAmount, sashimiReserve, wethReserve);
-        wethSashimiPair.swap(amountOutput, uint(0), to, new bytes(0));
+        WETH_SASHIMI_PAIR.swap(amountOutput, uint(0), to, new bytes(0));
         return amountOutput;
     }
-    
+
     // Pool info
     function lockableToken(uint256 poolId) external view override returns (IERC20) {
-        (IERC20 lpToken,,,) = sashimiMasterChef.poolInfo(poolId);
+        (IERC20 lpToken,,,) = SASHIMI_MASTERCHEF.poolInfo(poolId);
         return lpToken;
     }
 
     function lockedAmount(address user, uint256 poolId) external view override returns (uint256) {
-        (uint256 amount,) = sashimiMasterChef.userInfo(poolId, user);
+        (uint256 amount,) = SASHIMI_MASTERCHEF.userInfo(poolId, user);
         return amount;
     }
-    
+
     // Pool actions, requires impersonation via delegatecall
     function deposit(address _adapter, uint256 poolId, uint256 amount) external override {
         IVampireAdapter adapter = IVampireAdapter(_adapter);
-        adapter.lockableToken(poolId).approve(address(sashimiMasterChef), uint256(-1));
-        sashimiMasterChef.deposit(poolId, amount);
+        adapter.lockableToken(poolId).approve(address(SASHIMI_MASTERCHEF), uint256(-1));
+        SASHIMI_MASTERCHEF.deposit(poolId, amount);
     }
 
     function withdraw(address, uint256 poolId, uint256 amount) external override {
-        sashimiMasterChef.withdraw(poolId, amount);
+        SASHIMI_MASTERCHEF.withdraw(poolId, amount);
     }
 
     function claimReward(address, uint256 poolId) external override {
-        sashimiMasterChef.deposit(poolId, 0);
+        SASHIMI_MASTERCHEF.deposit(poolId, 0);
     }
-    
+
     function emergencyWithdraw(address, uint256 poolId) external override {
-        sashimiMasterChef.emergencyWithdraw(poolId);
+        SASHIMI_MASTERCHEF.emergencyWithdraw(poolId);
     }
 
     // Service methods
     function poolAddress(uint256) external view override returns (address) {
-        return address(sashimiMasterChef);
+        return address(SASHIMI_MASTERCHEF);
     }
 
     function rewardToWethPool() external view override returns (address) {
-        return address(wethSashimiPair);
+        return address(WETH_SASHIMI_PAIR);
     }
-    
+
     function lockedValue(address, uint256) external override view returns (uint256) {
         require(false, "not implemented");
-    }    
+    }
 
     function totalLockedValue(uint256) external override view returns (uint256) {
-        require(false, "not implemented"); 
+        require(false, "not implemented");
     }
 
     function normalizedAPY(uint256) external override view returns (uint256) {
